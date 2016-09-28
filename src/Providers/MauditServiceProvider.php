20761dd6edbd4bb52ab99e0c5cc686c7075e9d2e
Irineu Junior <irineu@mixd.com.br>
2016-09-28 18:08:37 +0000

    Troca do Facade do menu

--- src/Providers/MauditServiceProvider.php
+++ src/Providers/MauditServiceProvider.php
@@ -3,8 +3,7 @@
 namespace Mixdinternet\Maudit\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
-use Menu;
+use Pingpong\Menus\MenuFacade as Menu;
 
 class MauditServiceProvider extends ServiceProvider
 {
